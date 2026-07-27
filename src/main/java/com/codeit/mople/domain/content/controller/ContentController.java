c04e31bf2b0812d9912568bba44b419d1b11d9c5
김지성 <shes3332@naver.com>
2026-07-27 11:45:13 +0900

    콘텐츠 생성 API 컨트롤러 구현

--- src/main/java/com/codeit/mople/domain/content/controller/ContentController.java
+++ src/main/java/com/codeit/mople/domain/content/controller/ContentController.java
@@ -1,5 +1,33 @@
 package com.codeit.mople.domain.content.controller;
 
-public class ContentController {
+import com.codeit.mople.domain.content.dto.ContentCreateRequest;
+import com.codeit.mople.domain.content.dto.ContentResponse;
+import com.codeit.mople.domain.content.service.ContentService;
+import jakarta.validation.Valid;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+@RestController
+@RequestMapping("/api/contents")
+@RequiredArgsConstructor
+public class ContentController {
+  private final ContentService contentService;
+
+  @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+  public ResponseEntity<ContentResponse> createContent(
+      @RequestHeader("X-User-Id")UUID adminId,
+      @Valid @RequestPart("request")ContentCreateRequest request,
+      @RequestPart(value = "thumbnail", required = false) MultipartFile thumbnail) {
+    ContentResponse response = contentService.createContent(adminId, request, thumbnail);
+    return ResponseEntity.status(HttpStatus.CREATED).body(response);
+  }
 }
--- src/main/java/com/codeit/mople/domain/content/service/ContentServiceImpl.java
+++ src/main/java/com/codeit/mople/domain/content/service/ContentServiceImpl.java
@@ -1,5 +1,19 @@
 package com.codeit.mople.domain.content.service;
 
-public class ContentServiceImpl {
+import com.codeit.mople.domain.content.dto.ContentCreateRequest;
+import com.codeit.mople.domain.content.dto.ContentResponse;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+@Service
+@RequiredArgsConstructor
+public class ContentServiceImpl implements ContentService {
+
+  @Override
+  public ContentResponse createContent(UUID adminId, ContentCreateRequest request,
+      MultipartFile thumbnail) {
+    return null;
+  }
 }
