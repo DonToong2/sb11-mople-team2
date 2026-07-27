c6c0c202060dd2d2a8bde60cff784730be8bd972
김명근 <rlaaudrms369@gmail.com>
2026-07-27 10:23:10 +0900

    feat: Playlist 엔티티 생성(정적 팩토리 메서드 포함)

---
+++ src/main/java/com/codeit/mople/domain/playlist/entity/Playlist.java
@@ -0,0 +1,42 @@
+package com.codeit.mople.domain.playlist.entity;
+
+import com.codeit.mople.global.entity.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "playlists")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Playlist extends BaseTimeEntity {
+
+  // userId
+  @Column(nullable = false)
+  private UUID ownerId;
+
+  @Column(nullable = false)
+  private String title;
+
+  @Column(nullable = false)
+  private String description;
+
+  @Column(nullable = false)
+  private long subscriberCount;
+
+  private Playlist(UUID ownerId, String title, String description) {
+    this.ownerId = ownerId;
+    this.title = title;
+    this.description = description;
+    this.subscriberCount = 0L;
+  }
+
+  public static Playlist create(UUID ownerId, String title, String description) {
+    return new Playlist(ownerId, title, description);
+  }
+
+}
