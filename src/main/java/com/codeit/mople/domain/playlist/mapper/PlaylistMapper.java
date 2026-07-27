3a0faf7c5dee854658bc98d390c23c98dd8a32c4
김명근 <rlaaudrms369@gmail.com>
2026-07-27 12:02:57 +0900

    feat: PlaylistOwnerResponse에 대한 매퍼 클래스 작성

--- src/main/java/com/codeit/mople/domain/playlist/mapper/PlaylistMapper.java
+++ src/main/java/com/codeit/mople/domain/playlist/mapper/PlaylistMapper.java
@@ -5,11 +5,14 @@ import com.codeit.mople.domain.playlist.dto.response.PlaylistOwnerResponse;
 import com.codeit.mople.domain.playlist.dto.response.PlaylistResponse;
 import com.codeit.mople.domain.playlist.entity.Playlist;
 import java.util.List;
+import org.springframework.stereotype.Component;
 
+@Component
 public class PlaylistMapper {
 
   public PlaylistResponse toResponse(
-      Playlist playlist, PlaylistOwnerResponse owner,
+      Playlist playlist,
+      PlaylistOwnerResponse owner,
       boolean subscribedByMe,
       List<PlaylistContentResponse> contents
   ) {
