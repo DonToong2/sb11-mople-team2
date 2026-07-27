3ce8c203cc000ad02c0ed9120e8b047323f803dd
김명근 <rlaaudrms369@gmail.com>
2026-07-27 19:46:12 +0900

    refactor: Bean Validation에 메시지 추가

--- src/main/java/com/codeit/mople/domain/playlist/dto/request/PlaylistCreateRequest.java
+++ src/main/java/com/codeit/mople/domain/playlist/dto/request/PlaylistCreateRequest.java
@@ -3,10 +3,10 @@ package com.codeit.mople.domain.playlist.dto.request;
 import jakarta.validation.constraints.NotBlank;
 
 public record PlaylistCreateRequest(
-    @NotBlank
+    @NotBlank(message = "제목을 입력해주세요.")
     String title,
 
-    @NotBlank
+    @NotBlank(message = "설명을 입력해주세요.")
     String description
 ) {
 
