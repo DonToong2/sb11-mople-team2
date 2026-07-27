4473eb0ede2bf75b25195817691f43fc23d70df4
김명근 <rlaaudrms369@gmail.com>
2026-07-27 13:59:49 +0900

    feat: Playlist 컨트롤러 클래스 생성 및 PlaylistController.create() 메서드 구현

---
+++ src/main/java/com/codeit/mople/domain/playlist/controller/PlaylistController.java
@@ -0,0 +1,35 @@
+package com.codeit.mople.domain.playlist.controller;
+
+import com.codeit.mople.domain.playlist.dto.request.PlaylistCreateRequest;
+import com.codeit.mople.domain.playlist.dto.response.PlaylistResponse;
+import com.codeit.mople.domain.playlist.service.PlaylistService;
+import jakarta.validation.Valid;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/playlists")
+@RequiredArgsConstructor
+public class PlaylistController {
+
+  private final PlaylistService playlistService;
+
+  @PostMapping
+  public ResponseEntity<PlaylistResponse> create(
+      @RequestParam UUID ownerId,
+      @Valid @RequestBody PlaylistCreateRequest request
+  ) {
+
+    PlaylistResponse response = playlistService.create(ownerId, request);
+
+    return ResponseEntity.status(HttpStatus.CREATED).body(response);
+  }
+
+}
