b142dde57b53256814cd2fb52b9409996680d72c
김지성 <shes3332@naver.com>
2026-07-28 10:22:39 +0900

    feat: 콘텐츠 단건 조회 API 컨트롤러 구현

--- src/main/java/com/codeit/mople/domain/content/controller/ContentController.java
+++ src/main/java/com/codeit/mople/domain/content/controller/ContentController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +26,7 @@ import org.springframework.web.multipart.MultipartFile;
 public class ContentController {
   private final ContentService contentService;
 
+  //콘텐츠 생성
   @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
   public ResponseEntity<ContentResponse> createContent(
       @RequestHeader("X-User-Id")UUID adminId,
@@ -34,12 +36,21 @@ public class ContentController {
     return ResponseEntity.status(HttpStatus.CREATED).body(response);
   }
 
+  //콘텐츠 목록 조회
   @GetMapping
   public ResponseEntity<ContentPageResponse> getContents(
       @RequestParam("limit") int limit,
       @RequestParam("sortDirection") String sortDirection,
       @RequestParam("sortBy") String sortBy) {
     ContentPageResponse response = contentService.getContents(limit, sortDirection, sortBy);
+    return ResponseEntity.ok(response);
+  }
+
+  //콘텐츠 단건 조회
+  @GetMapping("/{contentId}")
+  public ResponseEntity<ContentResponse> getContent(
+      @PathVariable UUID contentId) {
+    ContentResponse response = contentService.getContent(contentId);
     return ResponseEntity.ok(response);
   }
 }
