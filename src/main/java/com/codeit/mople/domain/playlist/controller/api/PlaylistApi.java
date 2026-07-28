44b7c08ab793213528d64a9e41c659784471bcaa
eomoff <eomoff@gmail.com>
2026-07-28 22:51:18 +0900

    feat: 플레이리스트 구독 API 명세 작성

--- src/main/java/com/codeit/mople/domain/playlist/controller/api/PlaylistApi.java
+++ src/main/java/com/codeit/mople/domain/playlist/controller/api/PlaylistApi.java
@@ -11,6 +11,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.UUID;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -60,5 +62,41 @@ public interface PlaylistApi {
   ResponseEntity<PlaylistResponse> create(
       @RequestParam UUID ownerId,
       @Valid @RequestBody PlaylistCreateRequest request
+  );
+
+  // service 코드 작성하면서 수정 예정
+  @Operation(
+      summary = "플레이리스트 구독"
+  )
+  @ApiResponses({
+      @ApiResponse(
+          responseCode = "200",
+          description = "성공",
+          content = @Content(schema = @Schema(implementation = com.codeit.mople.global.response.ApiResponse.class))
+      ),
+      @ApiResponse(
+          responseCode = "204",
+          description = "성공",
+          content = @Content(schema = @Schema(implementation = com.codeit.mople.global.response.ApiResponse.class))
+      ),
+      @ApiResponse(
+          responseCode = "400",
+          description = "잘못된 요청",
+          content = @Content(schema = @Schema(implementation = com.codeit.mople.global.response.ApiResponse.class))
+      ),
+      @ApiResponse(
+          responseCode = "401",
+          description = "인증 오류",
+          content = @Content(schema = @Schema(implementation = com.codeit.mople.global.response.ApiResponse.class))
+      ),
+      @ApiResponse(
+          responseCode = "500",
+          description = "서버 오류",
+          content = @Content(schema = @Schema(implementation = com.codeit.mople.global.response.ApiResponse.class))
+      ),
+  })
+  ResponseEntity<Void> subscribe(
+     @PathVariable UUID playlistId,
+     @RequestParam UUID subscriberId
   );
 }
