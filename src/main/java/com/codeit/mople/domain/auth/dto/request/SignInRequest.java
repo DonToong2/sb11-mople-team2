3b0da2210bc7febb588e86ef3b8edfa135a20f39
apple <apple@MacBook-Pro.local>
2026-07-28 21:19:19 +0900

    feat: 인증 도메인 DTO, ErrorCode, CustomUserDetails 추가(#16)

--- src/main/java/com/codeit/mople/domain/auth/dto/request/SignInRequest.java
+++ src/main/java/com/codeit/mople/domain/auth/dto/request/SignInRequest.java
@@ -1,4 +1,13 @@
 package com.codeit.mople.domain.auth.dto.request;
 
-public record SignInRequest() {
-}
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record SignInRequest(
+    @NotBlank
+    @Email
+    String email,
+
+    @NotBlank
+    String password
+) {}
--- src/main/java/com/codeit/mople/domain/auth/security/CustomUserDetails.java
+++ src/main/java/com/codeit/mople/domain/auth/security/CustomUserDetails.java
@@ -1,4 +1,37 @@
 package com.codeit.mople.domain.auth.security;
 
-public class CustomUserDetails {
+import com.codeit.mople.domain.user.entity.Role;
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Getter
+public class CustomUserDetails implements UserDetails {
+
+  private final UUID userId;
+  private final Role role;
+
+  public CustomUserDetails(UUID userId, Role role) {
+    this.userId = userId;
+    this.role = role;
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+  }
+
+  @Override
+  public String getPassword() {
+    return null;
+  }
+
+  @Override
+  public String getUsername() {
+    return userId.toString();
+  }
 }
