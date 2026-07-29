ff1ba245586f8f6090c817823e36caa42f884c1b
vincent865 <tkrhdtkfkdgo@gmail.com>
2026-07-29 09:35:34 +0900

    feat: 테스트 ã시 테스트 계정 추가

--- src/test/java/com/codeit/final_project/FinalProjectApplicationTests.java
+++ src/test/java/com/codeit/final_project/FinalProjectApplicationTests.java
@@ -1,9 +1,12 @@
 package com.codeit.final_project;
 
+import com.codeit.mople.MopleApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(classes = MopleApplication.class)
+@ActiveProfiles("test")
 class FinalProjectApplicationTests {
 
   @Test
