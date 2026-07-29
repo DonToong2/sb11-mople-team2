13442ebb1432c551653f5a7e38ee591b45ddf7e8
김명근 <rlaaudrms369@gmail.com>
2026-07-29 13:48:34 +0900

    refactor: 데일리스크럼 후 Swagger 문서 미사용으로 설정

--- src/main/java/com/codeit/mople/domain/review/controller/ReviewController.java
+++ src/main/java/com/codeit/mople/domain/review/controller/ReviewController.java
@@ -1,30 +1,26 @@
 package com.codeit.mople.domain.review.controller;
 
 import com.codeit.mople.domain.auth.security.CustomUserDetails;
-import com.codeit.mople.domain.review.controller.api.ReviewApi;
 import com.codeit.mople.domain.review.dto.request.ReviewCreateRequest;
 import com.codeit.mople.domain.review.dto.response.ReviewResponse;
 import com.codeit.mople.domain.review.service.ReviewService;
 import jakarta.validation.Valid;
 import java.net.URI;
-import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/reviews")
 @RequiredArgsConstructor
-public class ReviewController implements ReviewApi {
+public class ReviewController {
 
   private final ReviewService reviewService;
 
-  @Override
   @PostMapping
   public ResponseEntity<ReviewResponse> create(
       @AuthenticationPrincipal(errorOnInvalidType = true) CustomUserDetails userDetails,
