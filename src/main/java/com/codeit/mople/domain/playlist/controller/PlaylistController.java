40214f4350a86ef366ba78db8504c7ed60abbdb5
김명근 <rlaaudrms369@gmail.com>
2026-07-29 19:22:56 +0900

    feat: PlaylistController.update() 메서드 구현

--- src/main/java/com/codeit/mople/domain/playlist/controller/PlaylistController.java
+++ src/main/java/com/codeit/mople/domain/playlist/controller/PlaylistController.java
@@ -1,7 +1,9 @@
 package com.codeit.mople.domain.playlist.controller;
 
+import com.codeit.mople.domain.auth.security.CustomUserDetails;
 import com.codeit.mople.domain.playlist.controller.api.PlaylistApi;
 import com.codeit.mople.domain.playlist.dto.request.PlaylistCreateRequest;
+import com.codeit.mople.domain.playlist.dto.request.PlaylistUpdateRequest;
 import com.codeit.mople.domain.playlist.dto.response.PlaylistResponse;
 import com.codeit.mople.domain.playlist.service.PlaylistService;
 import com.codeit.mople.domain.user.entity.User;
@@ -13,7 +15,9 @@ import java.net.URI;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -38,7 +42,7 @@ public class PlaylistController implements PlaylistApi {
 
     // TODO 김명근: Custom UserDetails 등 인증 구현 시 해당 findById 삭제(UserRepository Import도 같이 삭제)
     User owner = userRepository.findById(ownerId).orElseThrow(() ->
-            new CustomException(UserErrorCode.USER_NOT_FOUND));
+        new CustomException(UserErrorCode.USER_NOT_FOUND));
 
     PlaylistResponse response = playlistService.create(owner, request);
 
@@ -52,6 +56,18 @@ public class PlaylistController implements PlaylistApi {
       @PathVariable UUID playlistId
   ) {
     PlaylistResponse response = playlistService.find(playlistId);
+
+    return ResponseEntity.ok(response);
+  }
+
+  @PatchMapping("/{playlistId}")
+  public ResponseEntity<PlaylistResponse> update(
+      @PathVariable UUID playlistId,
+      @RequestBody PlaylistUpdateRequest request,
+      @AuthenticationPrincipal CustomUserDetails userDetails
+  ) {
+    PlaylistResponse response =
+        playlistService.update(playlistId, request, userDetails.getUserId());
 
     return ResponseEntity.ok(response);
   }
