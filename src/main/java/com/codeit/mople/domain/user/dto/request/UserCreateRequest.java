9045c52697114a663835fd4e561507ae9607ecf4
apple <apple@MacBook-Pro.local>
2026-07-29 18:08:20 +0900

    feat: 요청 DTO에 한글 검증 메시지 추가(#25)

--- src/main/java/com/codeit/mople/domain/user/dto/request/UserCreateRequest.java
+++ src/main/java/com/codeit/mople/domain/user/dto/request/UserCreateRequest.java
@@ -5,14 +5,14 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record UserCreateRequest(
-    @NotBlank
-    @Email
+    @NotBlank(message = "이메일을 입력해주세요.")
+    @Email(message = "올바른 이메일 형식이 아닙니다.")
     String email,
 
-    @NotBlank
-    @Size(min = 6,  max = 20)
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+    @Size(min = 6,  max = 20, message = "패스워드는 최소 6자, 최대 20자여야 합니다.")
     String password,
 
-    @NotBlank
+    @NotBlank(message = "이름을 입력해주세요.")
     String name
 ) {}
