a5eddd691798738b6ce8ad5b2784f1d73bc7857a
DonToong2 <rlaaudrms369@gmail.com>
2026-07-31 17:11:25 +0900

    feat: Review 수정/삭제 API 구현 (#49)
    * refactor: 사용자 이름 갱신 시 1~20자의 공백은 들어오지 않도록 개선
    
    * feat: 리뷰 내용 또는 별점을 수정하는 update() 메서드 작성
    
    * test: Review.update()에 대한 테스트 코드 작성
    
    * feat: Review 중간 도메인 계층 예외 클래스 및 ReviewErrorCode 생성
    
    * feat: ReviewService.update() 메서드에서 요청 파라미터를 받아오기 위한 ReviewUpdateRequest 생성
    
    * fix: Bean Validation에 메시지 추가하여 GlobalException에서 메시지를 호출하도록 수정
    
    * fix: ReviewErrorCode 필드에 final을 붙여 생성자 주입(컴파일러 에러 해결)
    
    * feat: ReviewService.update() 메서드 작성
    
    * test: ReviewService.update() 메서드에 대한 성공/실패 테스트 코드 작성
    
    * refactor: ReviewService.update() 메서드에 로깅 메시지 추가
    
    * feat: ReviewController.update() 메서드 구현
    
    * fix: authorId는 인증 객체의 userId를 받아오도록 수정
    
    * fix: PATCH 요청이기 때문에 평점 필드의 `@NotNull` 제거
    
    * fix: 접근권한이 없는 에러코드에 대한 상태코드 올바르게 수정
    
    * test: ReviewController.update() 메서드에 대한 성공/실패 테스트 코드 작성
    
    * test: Review 수정 API 테스트 코드 작성
    
    * feat: ReviewService.delete() 메서드 구현
    
    * test: ReviewService.delete() 메서드에 대한 테스트 코드 작성
    
    * refactor: ReviewService.delete() 메서드에 로깅 메시지 추가
    
    * feat: ReviewController.delete() 메서드 구현
    
    * test: ReviewController.delete() 메서드에 대한 테스트 코드 작성
    
    * test: Review 삭제 API 테스트 코드 작성
    
    * test: 404 실패 테스트와 생성 401 실패 테스트에 불필요한 DB 검증 메서드 대신 의미있는 DB 검증 메서드를 호출하도록 개선
    
    * fix: 리뷰 내용에 줄바꿈 허용하도록 수정
    
    * fix: 에러코드 접두사명을 올바르게 변경
    
    * chore: 리뷰 삭제 완료 시 로그 메시지 오타 수정
    
    * test: 테스트 메서드명 올바르게 수정
    
    * fix: 리뷰가 존재하지 않을 때 로깅 메시지에 콘텐츠의 평균 평점이 0.0점으로 나오도록 수정

--- src/test/java/com/codeit/mople/domain/review/entity/ReviewTest.java
+++ src/test/java/com/codeit/mople/domain/review/entity/ReviewTest.java
@@ -6,36 +6,118 @@ import com.codeit.mople.domain.content.entity.Content;
 import com.codeit.mople.domain.content.entity.ContentType;
 import com.codeit.mople.domain.user.entity.User;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class ReviewTest {
 
-  @Test
-  @DisplayName("리뷰 생성 성공")
-  void create_success() {
-    // given
-    User user = User.createUser("test@test.com", "12345678", "test");
-    Content content = new Content(
+  private User author;
+  private Content content;
+
+  private String text;
+  private double rating;
+
+  private Review review;
+  private String newText;
+  private double newRating;
+
+  @BeforeEach
+  void setUp() {
+    author = User.createUser("test@test.com", "12345678", "test");
+    content = new Content(
         ContentType.DRAMA,
         "test",
         "test 콘텐츠",
         "test/image.png",
         List.of("테스트")
     );
-    User author = User.createUser("test@test.com", "12345678", "test");
 
-    String text = "리뷰 내용";
-    double rating = 5.0;
+    text = "리뷰 내용";
+    rating = 5.0;
+  }
 
-    // when
-    Review review = Review.create(content, author, text, rating);
+  @Nested
+  @DisplayName("리뷰 생성")
+  class Create {
 
-    // then
-    assertThat(review.getContent()).isEqualTo(content);
-    assertThat(review.getAuthor()).isEqualTo(author);
-    assertThat(review.getText()).isEqualTo(text);
-    assertThat(review.getRating()).isEqualTo(rating);
+    @Test
+    @DisplayName("리뷰 생성 성공")
+    void create_success() {
+      // given
+
+      // BeforeEach에서 user, author, content, text, rating을 초기화
+
+      // when
+      Review review = Review.create(content, author, text, rating);
+
+      // then
+      assertThat(review.getContent()).isEqualTo(content);
+      assertThat(review.getAuthor()).isEqualTo(author);
+      assertThat(review.getText()).isEqualTo(text);
+      assertThat(review.getRating()).isEqualTo(rating);
+
+    }
+
+  }
+
+  @Nested
+  @DisplayName("리뷰 수정")
+  class Update {
+
+    @BeforeEach
+    void setUp() {
+      review = Review.create(content, author, text, rating);
+
+      newText = "수정한 내용";
+      newRating = 3.0;
+    }
+
+    @Test
+    @DisplayName("리뷰 수정 성공 - 내용, 평점 둘 다 수정")
+    void update_success() {
+      // given
+
+      // BeforeEach에서 user, author, content, text, rating을 통해 review와 newText, newRating을 초기화
+
+      // when
+      review.update(newText, newRating);
+
+      // then
+      assertThat(review.getText()).isEqualTo(newText);
+      assertThat(review.getRating()).isEqualTo(newRating);
+    }
+
+    @Test
+    @DisplayName("리뷰 수정 성공 - 내용만 수정")
+    void update_success_onlyText() {
+      // given
+
+      // BeforeEach에서 user, author, content, text, rating을 통해 review와 newText을 초기화
+
+      // when
+      review.update(newText, null);
+
+      // then
+      assertThat(review.getText()).isEqualTo(newText);
+      assertThat(review.getRating()).isEqualTo(rating);
+    }
+
+    @Test
+    @DisplayName("리뷰 수정 성공 - 평점만 수정")
+    void update_success_onlyRating() {
+      // given
+
+      // BeforeEach에서 user, author, content, text, rating을 통해 review와 newRating을 초기화
+
+      // when
+      review.update(null, newRating);
+
+      // then
+      assertThat(review.getText()).isEqualTo(text);
+      assertThat(review.getRating()).isEqualTo(newRating);
+    }
 
   }
 
