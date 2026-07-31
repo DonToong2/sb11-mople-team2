78c88aa42b8225b930994338c0fee19247e87ca4
김명근 <rlaaudrms369@gmail.com>
2026-07-31 17:09:18 +0900

    feat: PlaylistRepository QueryDSL 사용을 위해 JPAQueryFactory 주입을 위한 임시 QueryDslConfig 생성

--- src/main/java/com/codeit/mople/domain/playlist/repository/PlaylistRepository.java
+++ src/main/java/com/codeit/mople/domain/playlist/repository/PlaylistRepository.java
@@ -1,9 +1,12 @@
 package com.codeit.mople.domain.playlist.repository;
 
 import com.codeit.mople.domain.playlist.entity.Playlist;
+import com.codeit.mople.domain.playlist.repository.querydsl.PlaylistCustomRepository;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PlaylistRepository extends JpaRepository<Playlist, UUID>, PlaylistRepositoryCustom {
+public interface PlaylistRepository extends
+    JpaRepository<Playlist, UUID>,
+    PlaylistCustomRepository {
 
 }
--- src/main/java/com/codeit/mople/domain/playlist/repository/querydsl/PlaylistCustomRepository.java
+++ src/main/java/com/codeit/mople/domain/playlist/repository/querydsl/PlaylistCustomRepository.java
@@ -1,10 +1,10 @@
-package com.codeit.mople.domain.playlist.repository;
+package com.codeit.mople.domain.playlist.repository.querydsl;
 
 import com.codeit.mople.domain.playlist.dto.request.PlaylistQueryCondition;
 import com.codeit.mople.domain.playlist.entity.Playlist;
 import java.util.List;
 
-public interface PlaylistRepositoryCustom {
+public interface PlaylistCustomRepository {
 
   List<Playlist> findAll(PlaylistQueryCondition condition);
 
