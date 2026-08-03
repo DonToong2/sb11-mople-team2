52664d08bbbe8a00910e49c396e7f2223cb56766
김지성 <shes3332@naver.com>
2026-08-03 15:48:35 +0900

    refactor: WatchingSession 엔티티에 복합 유니크 제약조건 추가

--- src/main/java/com/codeit/mople/domain/watchingsession/entity/WatchingSession.java
+++ src/main/java/com/codeit/mople/domain/watchingsession/entity/WatchingSession.java
@@ -8,15 +8,18 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "watching_sessions")
+@Table(name = "watching_sessions", uniqueConstraints = {
+    @UniqueConstraint(name = "uk_user_content", columnNames = {"user_id", "content_id"})})
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class WatchingSession extends BaseEntity {
+
   //유저 - N:1 연관관계
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "user_id", nullable = false)
