3c475ad9643fabd5f0a3f4f3a657b9c02af5d9fb
김지성 <shes3332@naver.com>
2026-08-04 17:13:01 +0900

    feat: SportsDB Feign 통신 로깅을 위한 Config 설정 추가

---
+++ src/main/java/com/codeit/mople/domain/content/client/sportsdb/SportsDbFeignConfig.java
@@ -0,0 +1,14 @@
+package com.codeit.mople.domain.content.client.sportsdb;
+
+import feign.Logger;
+import feign.Logger.Level;
+import org.springframework.context.annotation.Bean;
+
+public class SportsDbFeignConfig {
+
+  //외부 API 통신 요청/응답의 모든 헤더와 바디를 로그로 출력
+  @Bean
+  public Logger.Level feignLoggerLevel() {
+    return Level.FULL;
+  }
+}
