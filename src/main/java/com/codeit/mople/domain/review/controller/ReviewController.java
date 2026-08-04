914946761bb9f32053f2918629aa898876989156
김명근 <rlaaudrms369@gmail.com>
2026-08-04 11:51:58 +0900

    feat: ReviewController.findAll() 메서드 구현

--- src/main/java/com/codeit/mople/domain/review/controller/ReviewController.java
+++ src/main/java/com/codeit/mople/domain/review/controller/ReviewController.java
@@ -2,7 +2,9 @@ package com.codeit.mople.domain.review.controller;
 
 import com.codeit.mople.domain.auth.security.CustomUserDetails;
 import com.codeit.mople.domain.review.dto.request.ReviewCreateRequest;
+import com.codeit.mople.domain.review.dto.request.ReviewQueryCondition;
 import com.codeit.mople.domain.review.dto.request.ReviewUpdateRequest;
+import com.codeit.mople.domain.review.dto.response.ReviewCursorResponse;
 import com.codeit.mople.domain.review.dto.response.ReviewResponse;
 import com.codeit.mople.domain.review.service.ReviewService;
 import jakarta.validation.Valid;
@@ -12,6 +14,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,12 +35,20 @@ public class ReviewController {
       @AuthenticationPrincipal(errorOnInvalidType = true) CustomUserDetails userDetails,
       @Valid @RequestBody ReviewCreateRequest request
   ) {
-
     ReviewResponse response = reviewService.create(userDetails.getUserId(), request);
 
     return ResponseEntity
         .created(URI.create("/api/reviews/" + response.id()))
         .body(response);
+  }
+
+  @GetMapping
+  public ResponseEntity<ReviewCursorResponse> findAll(
+      @Valid @ModelAttribute ReviewQueryCondition condition
+  ) {
+    ReviewCursorResponse response = reviewService.findAll(condition);
+
+    return ResponseEntity.ok(response);
   }
 
   @PatchMapping("/{reviewId}")
