0f91428a2ad2b858492c9275d81d16bc54f335d9
김지성 <shes3332@naver.com>
2026-08-07 09:55:52 +0900

    fix: MopleApplication 병합 과정에서 발생한 import 위치 및 컴파일 에러 수정

--- src/main/java/com/codeit/mople/MopleApplication.java
+++ src/main/java/com/codeit/mople/MopleApplication.java
@@ -2,21 +2,17 @@ package com.codeit.mople;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableFeignClients
 @EnableScheduling
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-
 @ConfigurationPropertiesScan
-@EnableFeignClients
 @SpringBootApplication
 public class MopleApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(MopleApplication.class, args);
   }
-
 }
