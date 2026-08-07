a2f2b1cade76021eb3da9131f809b166239dd500
eomoff <eomoff@gmail.com>
2026-08-07 13:21:37 +0900

    fix: SportsDB 머지로 Job 빈이 늘어 끊긴 JobLauncherTestUtils 주입 명시화

--- src/test/java/com/codeit/mople/domain/content/client/tmdb/config/TmdbCollectJobConfigTest.java
+++ src/test/java/com/codeit/mople/domain/content/client/tmdb/config/TmdbCollectJobConfigTest.java
@@ -22,6 +22,7 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
@@ -31,6 +32,7 @@ import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.batch.test.JobRepositoryTestUtils;
 import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
@@ -53,11 +55,16 @@ public class TmdbCollectJobConfigTest {
   @Autowired
   private MeterRegistry meterRegistry;
 
+  @Autowired
+  @Qualifier("tmdbCollectJob")
+  private Job tmdbCollectJob;
+
   @MockitoBean
   private TmdbClient tmdbClient;
 
   @BeforeEach
   void setUp() {
+    jobLauncherTestUtils.setJob(tmdbCollectJob);
     jobRepositoryTestUtils.removeJobExecutions();
     contentRepository.deleteAll();
 
