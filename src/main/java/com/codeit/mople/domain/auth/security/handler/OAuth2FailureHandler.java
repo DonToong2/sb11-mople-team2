9447eaa79ecba9a78e7169d62e1b43cf08fdcbad
apple <apple@MacBook-Pro.local>
2026-08-08 10:31:35 +0900

    fix: 실패 리다이렉트에 고정된 오류 코드 추가(#96)

--- src/main/java/com/codeit/mople/domain/auth/security/handler/OAuth2FailureHandler.java
+++ src/main/java/com/codeit/mople/domain/auth/security/handler/OAuth2FailureHandler.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Component
 public class OAuth2FailureHandler extends SimpleUrlAuthenticationFailureHandler {
@@ -17,6 +18,10 @@ public class OAuth2FailureHandler extends SimpleUrlAuthenticationFailureHandler 
   @Override
   public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
       AuthenticationException exception) throws IOException {
-    getRedirectStrategy().sendRedirect(request, response, failureUri);
+    String targetUri = UriComponentsBuilder.fromUriString(failureUri)
+            .queryParam("error", "oauth2_authentication_failed")
+            .build()
+            .toUriString();
+    getRedirectStrategy().sendRedirect(request, response, targetUri);
   }
 }
