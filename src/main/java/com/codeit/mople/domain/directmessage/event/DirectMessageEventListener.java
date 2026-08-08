87bc0279e84f3282845b2904508ce4e0cdfcc686
김명근 <rlaaudrms369@gmail.com>
2026-08-08 10:10:34 +0900

    fix: SSE 전송 실패 시 DirectMessageService.sendMessage()가 성공해도 실패했다는 에러코드 반환을 방지하도록 설정(SSE 실시간 통신은 부가적인 기능으로 간주)

--- src/main/java/com/codeit/mople/domain/directmessage/event/DirectMessageEventListener.java
+++ src/main/java/com/codeit/mople/domain/directmessage/event/DirectMessageEventListener.java
@@ -37,14 +37,21 @@ public class DirectMessageEventListener {
 
     // TODO 김명근: kafka 도입 이후 topic 만들어서 비동기 처리(현재는 동기 상태)
     // SSE 이벤트 전송
-    sseService.send(
-        event.receiverId(),
-        "direct-messages",
-        directMessageDto
-    );
+    // RuntimeException, CustomException, IOException 등 다양한 예외가 발생할 수 있기 때문에 Exception으로 설정
+    // 대신 로그 메시지에 stackTrace를 확인하여 어떤 예외가 발생했는지 알 수 있도록 설정
+    try {
+      sseService.send(
+          event.receiverId(),
+          "direct-messages",
+          directMessageDto
+      );
 
-    log.info("SSE 전송 완료: receiverId={}, directMessageId={}",
-        event.receiverId(), event.directMessageId());
+      log.info("SSE 전송 완료: receiverId={}, directMessageId={}",
+          event.receiverId(), event.directMessageId());
+    } catch (Exception e) {
+      log.error("SSE 전송 실패: receiverId={}, directMessageId={}",
+          event.receiverId(), event.directMessageId(), e);
+    }
 
   }
 }
