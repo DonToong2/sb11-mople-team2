5d072b974a7e464585218931f3235ceea70bf583
Seongmin Kang <seongmin0244@hanyang.ac.kr>
2026-08-12 11:33:10 +0900

    refactor: WebSocketConfig의 와일드카드 CORS 보안 강화 - 환경변수(@Value) 사용하여 확인된 도메인의 접근만 허용

--- src/main/java/com/codeit/mople/global/config/WebSocketConfig.java
+++ src/main/java/com/codeit/mople/global/config/WebSocketConfig.java
@@ -2,7 +2,9 @@ package com.codeit.mople.global.config;
 
 import com.codeit.mople.domain.auth.security.JwtChannelInterceptor;
 import com.codeit.mople.global.error.CustomStompErrorHandler;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
@@ -18,11 +20,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
   private final JwtChannelInterceptor jwtChannelInterceptor;
   private final CustomStompErrorHandler customStompErrorHandler;
 
+  @Value("${app.websocket.allowed-origins:http://localhost:8080}")
+  private List<String> allowedOrigins;
+
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
     registry.addEndpoint("/ws")
         // TODO: 운영 단계에서는 실제 도메인(또는 IP) 주소 명시
-        .setAllowedOriginPatterns("*")
+        .setAllowedOrigins(allowedOrigins.toArray(new String[0]))
         .withSockJS();
 
     registry.setErrorHandler(customStompErrorHandler);
