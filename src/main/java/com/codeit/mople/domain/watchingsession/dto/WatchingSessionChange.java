e77e4e2c01d1307a11768617b66b2fbe1774c854
김지성 <shes3332@naver.com>
2026-08-13 18:43:20 +0900

    fix: 실시간 시청 세션 입장/퇴장 웹소켓 및 DTO 규격 동기화

--- src/main/java/com/codeit/mople/domain/watchingsession/dto/WatchingSessionChange.java
+++ src/main/java/com/codeit/mople/domain/watchingsession/dto/WatchingSessionChange.java
@@ -1,20 +1,15 @@
 package com.codeit.mople.domain.watchingsession.dto;
 
-import com.codeit.mople.global.dto.UserSummary;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record WatchingSessionChange(
-    String contentId,
-
-    @JsonProperty("user")
-    UserSummary user,
-
-    //이벤트 타입(ENTER / LEAVE)
     @JsonProperty("type")
     String type,
 
-    //실시간 시청자 수
-    @JsonProperty("count")
-    Long count
+    @JsonProperty("watchingSession")
+    WatchingSessionDetailDto watchingSession,
+
+    @JsonProperty("watcherCount")
+    Integer watcherCount
 ) {
 }
