908fa21f5da872dffe960335a7970b789bf22d15
김지성 <shes3332@naver.com>
2026-08-21 18:51:20 +0900

    refactor: 각 도메인 예외 클래스의 ErrorCode 파라미터 타입 구체화

--- src/main/java/com/codeit/mople/domain/auth/exception/AuthException.java
+++ src/main/java/com/codeit/mople/domain/auth/exception/AuthException.java
@@ -1,16 +1,15 @@
 package com.codeit.mople.domain.auth.exception;
 
 import com.codeit.mople.global.error.CustomException;
-import com.codeit.mople.global.error.ErrorCode;
 import java.util.Map;
 
 public class AuthException extends CustomException {
 
-  public AuthException(ErrorCode errorCode) {
+  public AuthException(AuthErrorCode errorCode) {
     super(errorCode);
   }
 
-  public AuthException(ErrorCode errorCode, Map<String, Object> details) {
+  public AuthException(AuthErrorCode errorCode, Map<String, Object> details) {
     super(errorCode, details);
   }
 }
