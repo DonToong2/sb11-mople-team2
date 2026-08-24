e8e3b0cbe4c64d1043a825dc9dc9ec3834a2c6e5
Seongmin Kang <seongmin0244@hanyang.ac.kr>
2026-08-24 15:30:42 +0900

    feat: Elasticsearch 인덱스(direct_messages)에 매핑되는 도큐먼트 엔티티 구현

---
+++ src/main/java/com/codeit/mople/domain/directmessage/document/DirectMessageDocument.java
@@ -0,0 +1,48 @@
+package com.codeit.mople.domain.directmessage.document;
+
+import com.codeit.mople.domain.directmessage.entity.DirectMessage;
+import java.time.Instant;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.data.elasticsearch.annotations.Setting;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Document(indexName = "direct_messages")
+@Setting(settingPath = "elastic-ngram-settings.json")
+public class DirectMessageDocument {
+
+  @Id
+  @Field(type = FieldType.Keyword)
+  private UUID id;
+
+  @Field(type = FieldType.Keyword)
+  private UUID conversationId;
+
+  @Field(type = FieldType.Keyword)
+  private UUID senderId;
+
+  @Field(type = FieldType.Text, analyzer = "ngram_analyzer")
+  private String content;
+
+  @Field(type = FieldType.Date)
+  private Instant createdAt;
+
+  public static DirectMessageDocument from (DirectMessage message) {
+    return new DirectMessageDocument(
+        message.getId(),
+        message.getConversation().getId(),
+        message.getSender().getId(),
+        message.getContent(),
+        message.getCreatedAt()
+    );
+  }
+}
