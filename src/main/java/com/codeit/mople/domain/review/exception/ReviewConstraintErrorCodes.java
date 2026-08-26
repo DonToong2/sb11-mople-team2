3abde2d8ddfa0c5797b471848df712b832022c54
김명근 <rlaaudrms369@gmail.com>
2026-08-26 19:00:14 +0900

    fix: 누락된 @Component 추가

--- src/main/java/com/codeit/mople/domain/review/exception/ReviewConstraintErrorCodes.java
+++ src/main/java/com/codeit/mople/domain/review/exception/ReviewConstraintErrorCodes.java
@@ -3,7 +3,9 @@ package com.codeit.mople.domain.review.exception;
 import com.codeit.mople.global.error.ConstraintErrorCodes;
 import com.codeit.mople.global.error.ErrorCode;
 import java.util.Map;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ReviewConstraintErrorCodes implements ConstraintErrorCodes {
   @Override
   public Map<String, ErrorCode> get() {
